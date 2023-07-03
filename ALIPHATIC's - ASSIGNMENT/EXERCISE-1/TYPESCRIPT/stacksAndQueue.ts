86669e78e094895f1d296d599a7ac001f9457972
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-03 18:06:23 +0100

    TS EX-2 Implement a Stack using a Linked List

--- ALIPHATIC's - ASSIGNMENT/EXERCISE-1/TYPESCRIPT/stacksAndQueue.ts
+++ ALIPHATIC's - ASSIGNMENT/EXERCISE-1/TYPESCRIPT/stacksAndQueue.ts
@@ -29,3 +29,60 @@ class Stack {
         return this.stack.length === 0;
     }
     }
+
+
+// 2. Implement a Stack using a Linked List
+class Node {
+    value: any;
+    next: Node | null;
+    constructor(value: any) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    private top: Node | null;
+    private bottom: Node | null;
+    private length: number;
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    
+    peek() {
+        return this.top;
+    }
+    
+    push(value: any) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+        this.top = newNode;
+        this.bottom = newNode;
+        } else {
+        const holdingPointer = this.top;
+        this.top = newNode;
+        this.top.next = holdingPointer;
+        }
+        this.length++;
+        return this;
+    }
+    
+    pop() {
+        if (!this.top) {
+        return null;
+        }
+        if (this.top === this.bottom) {
+        this.bottom = null;
+        }
+        const holdingPointer = this.top;
+        this.top = this.top.next;
+        this.length--;
+        return holdingPointer;
+    }
+    
+    isEmpty() {
+        return this.length === 0;
+    }
+    }
