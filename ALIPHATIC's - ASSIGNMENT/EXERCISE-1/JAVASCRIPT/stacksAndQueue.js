6e77a09560ddc1e8d4651eee7ef42daf9a2c2c33
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-03 13:41:41 +0100

    JavaScript EX-1. Implement a Stack using an Array

--- ALIPHATIC's - ASSIGNMENT/EXERCISE-1/JAVASCRIPT/stacksAndQueue.js
+++ ALIPHATIC's - ASSIGNMENT/EXERCISE-1/JAVASCRIPT/stacksAndQueue.js
@@ -5,3 +5,38 @@
  4. Implement a Queue using a Linked List
  5. Implement a Queue using a Doubly Linked List
  */
+
+
+
+// 1. Implement a Stack using an Array
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        if(this.items.length == 0)
+            return "Underflow";
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    printStack() {
+        var str = "";
+        for(var i = 0; i < this.items.length; i++)
+            str += this.items[i] + " ";
+        return str;
+    }
+}
+
