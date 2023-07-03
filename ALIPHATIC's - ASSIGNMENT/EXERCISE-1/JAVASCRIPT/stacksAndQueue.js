2879808c9ab6d8c13808735ed44bc172d1e0c89f
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-03 13:40:01 +0100

    Adding Javascript Exercise 1

---
+++ ALIPHATIC's - ASSIGNMENT/EXERCISE-1/JAVASCRIPT/stacksAndQueue.js
@@ -0,0 +1,7 @@
+/**
+ 1. Implement a Stack using an Array
+ 2. Implement a Stack using a Linked List
+ 3. Implement a Queue using an Array
+ 4. Implement a Queue using a Linked List
+ 5. Implement a Queue using a Doubly Linked List
+ */
